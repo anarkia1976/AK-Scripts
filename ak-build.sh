edec0c9c65f9f98e104ea084c1b72fbb15519902
anarkia1976 <stefano.villa1976@gmail.com>
2016-10-14 14:37:07 +0200

    ak-build: rework of public license

--- ak-build.sh
+++ ak-build.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
-
-##************************************************##
-##************************************************##
-## Kernel build script                            ##
-## author: @anarkia1976                           ##
-##                                                ##
-## Shell spinner function                         ##
-## author: tasos latsas                           ##
-##************************************************##
-##************************************************##
-
+#
+# AK Kernel build script
+#
+# Copyright (C) 2016 @anarkia1976
+#
+# spinner author: @tlatsas (https://github.com/tlatsas)
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 # colors
 green='\033[01;32m'
